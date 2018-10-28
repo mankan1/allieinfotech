18a16bbc80163bf8f3361817af22bc38d6f2a3f2
Manoj Kandlikar <manojkandlikar@fb.com>
2018-10-28 11:50:39 -0700

    services

--- app/company.component.js
+++ app/company.component.js
@@ -15,7 +15,7 @@ var CompanyComponent = (function () {
         this.router = router;
         this.title = 'INDUSTRY EXPERTISE';
         this.description = 'Many Industries, One Consumer';
-        this.content = 'We take pride in our strong vertical expertise. But we also know that insights from the solutions we have delivered help us to help all of our clients. Todays consumers expect each brand interaction to be seamless and intuitive, no matter the industry. We translate our cross-industry learnings into better solutions for all, because that's what we're best at - creating experiences for todays digital users.';
+        this.content = 'We help clients across all industries solve their toughest business problems. By combining domain-specific expertise - and a deep understanding of the consumer - we envision and deliver effortless experiences that drive loyalty, increase order volumes, improve adherence, and accomplish your goals.We take pride in our strong vertical expertise. But we also know that insights from the solutions we have delivered help us to help all of our clients. Todays consumers expect each brand interaction to be seamless and intuitive, no matter the industry. We translate our cross-industry learnings into better solutions for all, because thats what we are best at - creating experiences for todays digital users.';
     }
     CompanyComponent.prototype.ngOnInit = function () {
     };
--- ts/company.component.ts
+++ ts/company.component.ts
@@ -10,7 +10,7 @@ import { Router }           from '@angular/router-deprecated';
 export class CompanyComponent implements OnInit { 
     title = 'INDUSTRY EXPERTISE';
     description = 'Many Industries, One Consumer';
-    content = 'We help clients across all industries solve their toughest business problems. By combining domain-specific expertise - and a deep understanding of the consumer - we envision and deliver effortless experiences that drive loyalty, increase order volumes, improve adherence, and accomplish your goals.We take pride in our strong vertical expertise. But we also know that insights from the solutions we've delivered help us to help all of our clients. Today's consumers expect each brand interaction to be seamless and intuitive, no matter the industry. We translate our cross-industry learnings into better solutions for all, because that's what we're best at - creating experiences for today's digital users.';
+    content = 'We help clients across all industries solve their toughest business problems. By combining domain-specific expertise - and a deep understanding of the consumer - we envision and deliver effortless experiences that drive loyalty, increase order volumes, improve adherence, and accomplish your goals.We take pride in our strong vertical expertise. But we also know that insights from the solutions we have delivered help us to help all of our clients. Todays consumers expect each brand interaction to be seamless and intuitive, no matter the industry. We translate our cross-industry learnings into better solutions for all, because that's what we are best at - creating experiences for todays digital users.';
 
     constructor(private router: Router) { }
 
