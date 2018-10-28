66b4e13091c1fbcef6def7bfb4a8e72bd85a61e6
Manoj Kandlikar <manojkandlikar@fb.com>
2018-10-27 18:26:37 -0700

    test

--- index.js
+++ index.js
@@ -7,4 +7,13 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+    .get('/.well-known/acme-challenge/:content', function(req, res) {
+        var x = '9nbyuDS_ewIn_-5sTfBqFDuWxKIEf6hjzVIaG8HaS2o';
+        var y = 'F5Pl8rUw-xaXI5pkBadQGnX7SWZXvG9uBNY9tIu46B4';
+        if (y === req.params.content) {
+            res.send('F5Pl8rUw-xaXI5pkBadQGnX7SWZXvG9uBNY9tIu46B4.LB98ZAzz2-Ox5nBvnsCR2HpW5Pdp13hdgDU4LK1dTxw');
+        } else if (x === req.params.content) {
+            res.send('9nbyuDS_ewIn_-5sTfBqFDuWxKIEf6hjzVIaG8HaS2o.LB98ZAzz2-Ox5nBvnsCR2HpW5Pdp13hdgDU4LK1dTxw');
+        }
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
