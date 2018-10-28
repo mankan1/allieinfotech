d1752c1b32845e24454a61d86b33884c3b009e05
Manoj Kandlikar <manojkandlikar@fb.com>
2018-10-27 17:53:34 -0700

    test

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,16 @@ gulp.task('connect', function() {
             }
     });
 
+    app.get('/.well-known/acme-challenge/:content', function(req, res) {
+        var x = '9nbyuDS_ewIn_-5sTfBqFDuWxKIEf6hjzVIaG8HaS2o';
+        var y = 'F5Pl8rUw-xaXI5pkBadQGnX7SWZXvG9uBNY9tIu46B4';
+        if (y === req.params.content) {
+            res.send('F5Pl8rUw-xaXI5pkBadQGnX7SWZXvG9uBNY9tIu46B4.LB98ZAzz2-Ox5nBvnsCR2HpW5Pdp13hdgDU4LK1dTxw');
+        } else if (x === req.params.content) {
+            res.send('9nbyuDS_ewIn_-5sTfBqFDuWxKIEf6hjzVIaG8HaS2o.LB98ZAzz2-Ox5nBvnsCR2HpW5Pdp13hdgDU4LK1dTxw');
+        }
+    })
+
     connect.server({
       port: process.env.PORT || 8081,
       middleware: function(connect, opt) {
